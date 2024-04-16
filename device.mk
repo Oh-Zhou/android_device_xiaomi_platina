b7d3ebb39219ceaf114896d5024b45dcc33d8e79
Sabar <31942277+sabarop@users.noreply.github.com>
2024-04-16 11:10:51 +0700

    platina: Switch to android.hardware.usb@1.3-service.dual_role_usb
    Co-Authored-By: Michael Bestas <mikeNG@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -84,7 +84,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.3-service.dual_role_usb
 
 # Wifi
 PRODUCT_PACKAGES += \
