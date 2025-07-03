bb4547cbb9183e7cac3118ef906aa24e751e251d
sabarop <sabar.pakpahan@gmail.com>
2025-07-03 08:22:54 +0700

    platina: Initialize for Evolution X 11.0+

--- lineage_platina.mk
+++ lineage_platina.mk
@@ -19,8 +19,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Lineage stuff
+# Inherit some common Evolution-X stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_HAS_UDFPS := false
+TARGET_INCLUDE_ACCORD = false
+TARGET_SCREEN_WIDTH := 1080
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
+TARGET_ENABLE_BLUR := false
+TARGET_USES_PICO_GAPPS := true
 
 # Inherit from platina device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
