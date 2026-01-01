0c7740cb6e86349c4c684e2300569f2da8f42396
sabarop <sabar.pakpahan@gmail.com>
2026-01-01 09:27:11 +0700

    platina: Import audio amplifier from davinci

--- device.mk
+++ device.mk
@@ -13,6 +13,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
+PRODUCT_PACKAGES += \
+    audio_amplifier.platina
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.sdm660 \
