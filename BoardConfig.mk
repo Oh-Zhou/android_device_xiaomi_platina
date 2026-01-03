ab140ceec67dc5a16b3cb8b18d43c7e2d90d1e4e
sabarop <sabar.pakpahan@gmail.com>
2026-01-03 16:31:36 +0700

    platina: Enable AUDIO_FEATURE_ENABLED_TFA98XX_AMPLIFIER

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,9 @@ DEVICE_PATH := device/xiaomi/platina
 # Assert
 TARGET_OTA_ASSERT_DEVICE := platina
 
+# Audio
+AUDIO_FEATURE_ENABLED_TFA98XX_AMPLIFIER := true
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
