486d0d6c7675ea403a9208ebe8ad6e404b5a6773
sabarop <sabar.pakpahan@gmail.com>
2026-04-16 16:43:42 +0700

    Revert "platina: adapt to config fragments"
    This reverts commit ce1dd2f1ef63e8a9768f2efb2a35f1a3561f129f.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ TARGET_SCREEN_DENSITY := 440
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/xiaomi/platina.config
+TARGET_KERNEL_CONFIG := platina_defconfig
 
 # Partitions
 BOARD_SUPER_PARTITION_SYSTEM_DEVICE_SIZE := 3221225472
