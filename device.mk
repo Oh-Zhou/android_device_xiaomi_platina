210e7be944b0f12c3fce96c3b07198aff640a73c
sabarop <sabar.pakpahan@gmail.com>
2026-05-05 08:06:22 +0700

    platina: switch to xiaomi_sdm660 fingerprint service to aidl

--- device.mk
+++ device.mk
@@ -20,7 +20,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint-service.xiaomi \
+    android.hardware.biometrics.fingerprint-service.xiaomi_sdm660 \
     com.fingerprints.extension@1.0.vendor \
     vendor.goodix.hardware.fingerprintextension@1.0.vendor
 
