763520729bd0663f3ef9b74a6a4990d67691d384
Sabar <31942277+sabarop@users.noreply.github.com>
2026-08-29 09:16:56 +0700

    platina: Migrate QTI power HAL to soong_config_set

--- device.mk
+++ device.mk
@@ -83,6 +83,9 @@ PRODUCT_PACKAGES += \
     tune2fs_ramdisk \
     resize2fs_ramdisk
 
+# Power
+$(call soong_config_set,qtipower,tap_to_wake_node,/sys/touchpanel/double_tap)
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
