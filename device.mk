9efa528ab0da0d67320fcb58edffecbd4a73f0a0
Sabar <31942277+sabarop@users.noreply.github.com>
2026-08-29 09:13:41 +0700

    platina: Switch to AIDL USB Gadget HAL

--- device.mk
+++ device.mk
@@ -101,7 +101,7 @@ PRODUCT_SOONG_NAMESPACES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb \
-    android.hardware.usb.gadget@1.2-service-qti
+    android.hardware.usb.gadget-service.qti
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
